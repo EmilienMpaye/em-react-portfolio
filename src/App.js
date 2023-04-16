a4b01481cdc824725e8a7389afca1f19b07b2404
EmilienMpaye <mpayimanaemilien@gmail.com>
2023-04-16 16:24:55 +0200

    minor refactor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import SocialHide from "./components/SocialHide";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
